cf53686adbfc5d22e4b78613198d591ffd47422c
Mahdi Amini <mahdiaminik@gmail.com>
2023-09-26 13:38:41 +0330

    add udp

--- autossr/createhash.sh
+++ autossr/createhash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {1..20}
 do
-   j=$(printf '%s%02d\n' @Bcdef1234 $i)
+   j=$(printf '%s%02d\n' abcdef1234 $i)
    k=$(echo $j | md5sum | cut -c-10)
    printf '%02d %s %s\n' $i $k 'username'
 
